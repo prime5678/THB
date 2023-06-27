603088c8b49602a69a4dc77d18e2ca891645be68
Edwin Gregory <84358789+prime5678@users.noreply.github.com>
2023-06-27 16:35:27 -0400

    Update industrySelectScraper.py
    Was able to figure out a way that can pull the correct states, of course this method is contingent on certain ways that the original text is written but it is a good start. Having a harder time trying to parse and retrieve the correct institution name from the original text because it tends to partition text unnecessarily or recognize the goal entity under the wrong tag (e.g. Organization vs Person). I do not know how I am going to get the number of jobs, this could potentially be a task to start firsthand because it will take more time.

--- industrySelectScraper.py
+++ industrySelectScraper.py
@@ -4,7 +4,15 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+import nltk
+
+nltk.download('punkt')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+nltk.download('averaged_perceptron_tagger')
+
 def webPageParser(link):
+    listOfStates = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
     companyName = []
     state = []
     jobCount = []
@@ -27,8 +35,29 @@ def webPageParser(link):
     titles = titles[:-1]
     lst2 = []
     for x in zip(titles, lst):
-        lst2.append(x[0] + "\n" + x[1])
-    
-
+        lst2.append(x[1])
+    #above code extracts the text from the webpage and sorts the titles and corresponding paragraphs into a single list over which we can iterate and use methods to filter out specific information we need
+    #let's try to find a way for the program to recognize/distinguish between the features of company name, state, and jobs that will be created
+    #ideas: try using NLP or LLMs for retrieval
+    #idea 1: NLP in Python
+    for x in lst2:
+        templst = []
+        for sent in nltk.sent_tokenize(x):
+            for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
+                if hasattr(chunk, 'label'):
+                    templst.append(chunk.label() + " " + ' '.join(c[0] for c in chunk))
+        print(templst)
+        lstGPE = [s[4:] for s in templst if "GPE" in s]
+        lstPERSON = [s for s in templst if "PERSON" in s or "ORGANIZATION" in s]
+        if len(lstGPE) == 1:
+            state.append(lstGPE[0])
+        else:
+            p = [g for g in lstGPE if g in listOfStates]
+            state.append(p[0])
+        for q in range(len(lstPERSON)):
+            if "PERSON" in lstPERSON[q]:
+                lstPERSON[q] = lstPERSON[q][len("PERSON") + 1:]
+            else:
+                lstPERSON[q] = lstPERSON[q][len("ORGANIZATION") + 1:]
 
 webPageParser("https://www.industryselect.com/blog/new-us-manufacturing-plants-announced-may-2023")
